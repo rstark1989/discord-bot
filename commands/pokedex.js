9e6c31eb48d4b4a6ff98b515cd2856d8f754fa1b
nhcarrigan <nhcarrigan@gmail.com>
2020-05-31 19:52:27 -0700

    Add check for incorrect inputs.

--- commands/pokedex.js
+++ commands/pokedex.js
@@ -6,7 +6,19 @@ module.exports = {
   description: "Look up a Pokemon! Use the format |pokedex <number>.",
   command: async function(message) {
     const arguments = message.content.split(" ");
-    const number = arguments[1];
+    const number = parseInt(arguments[1]);
+    if (number < 1 || number > 807) {
+      message.channel.send(
+        `I apologise, ${message.author}, but I can only access information for Pokemon from number 1 to 807.`
+      );
+      return;
+    }
+    if (isNaN(number)) {
+      message.channel.send(
+        `I apologise, ${message.author}, but I can only search for Pokemon by number. Please try again.`
+      );
+      return;
+    }
     const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}`);
     const pokemon = await data.json();
     const pokemonEmbed = new Discord.MessageEmbed()
