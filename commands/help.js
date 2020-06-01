bced285b9144e971299ad92374d29f3096930a0f
nhcarrigan <nhcarrigan@gmail.com>
2020-06-01 15:09:18 -0700

    Update help command a bit.

--- commands/help.js
+++ commands/help.js
@@ -1,17 +1,22 @@
 const Discord = require("discord.js");
 const fs = require("fs");
+const config = require("../config.json");
+const prefix = config.prefix;
 
 module.exports = {
   //prefix and description - prefix is necessary to trigger command, description ensures it shows in |help.
   prefix: "help",
-  description: "Provides a list of current commands to the user.",
+  description:
+    "Provides a list of current commands to the user. Hey, that's THIS command! 🙃",
   command: async function(message) {
     //create message embed
     const user = message.author;
     const helpEmbed = new Discord.MessageEmbed()
       .setColor("#ab47e6")
       .setTitle("Bot Commands")
-      .setDescription("Hello! Here are my currently available commands.")
+      .setDescription(
+        `Hello! Here are my currently available commands. Remember that the command name must be prefixed with '${prefix}', just like the '${prefix}help' command you used to get this message.`
+      )
       .setFooter("Okay, that's all! Bye bye!");
     //read command files for prefix && description
     const files = await fs.promises.readdir(__dirname);
