a5efb0b7a5a3fb9dd4bbafc610b9b375b5500f70
nhcarrigan <nhcarrigan@gmail.com>
2020-07-13 13:35:06 -0700

    V 2.2.1 - change math command to return an embed.

--- src/commands/math.ts
+++ src/commands/math.ts
@@ -1,5 +1,6 @@
 import { commandInt } from "../interfaces/commandInt";
 import { evaluate } from "mathjs";
+import { MessageEmbed } from "discord.js";
 export const maths: commandInt = {
   prefix: "math",
   description: "Evaluates a mathematic expression and returns the value.",
@@ -7,9 +8,17 @@ export const maths: commandInt = {
     try {
       const expression = message.content.substring(5);
       const answer = evaluate(expression);
-      if (!answer)
+      if (!answer || !expression)
         return message.channel.send("ERROR 400: Invalid expression.");
-      message.channel.send(answer);
+      const mathEmbed = new MessageEmbed()
+        .setTitle("Calculation Protocol")
+        .setColor("#ab47e6")
+        .setDescription("BEEP BOOP: Calculation complete.")
+        .addFields(
+          { name: "Input", value: expression },
+          { name: "Result", value: answer }
+        );
+      message.channel.send(mathEmbed);
     } catch (error) {
       message.channel.send("ERROR 400: Invalid expression.");
     }
