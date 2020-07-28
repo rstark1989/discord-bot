f069e4c3fb9201abd15cbcc2d2cd7f690c53fd28
nhcarrigan <nhcarrigan@gmail.com>
2020-07-28 12:42:54 -0700

    Forgot to update the dnd help command

--- src/commands/dnd.ts
+++ src/commands/dnd.ts
@@ -8,7 +8,28 @@ export const dnd: commandInt = {
     const dndEmbed = new MessageEmbed()
       .setTitle("Dungeons and Dragons!")
       .setDescription("Search complete. Displaying available commands!")
-      .addFields({ name: "dndmon", value: "Gets information on a monster." });
+      .addFields(
+        {
+          name: "dndclass",
+          value: "Get information on a Dungeons and Dragons class",
+        },
+        {
+          name: "dndmon",
+          value: "Get information on a Dungeons and Dragons monster",
+        },
+        {
+          name: "dndrace",
+          value: "Get information on a Dungeons and Dragons race",
+        },
+        {
+          name: "dndschool",
+          value: "Get information on a Dungeons and Dragons school of magic",
+        },
+        {
+          name: "dndspell",
+          value: "Get information on a Dungeons and Dragons spell",
+        }
+      );
     message.channel.send(dndEmbed);
     return "success";
   },
