52693a4dcfb6e3aa6d12c95713d490bf9c342845
nhcarrigan <nhcarrigan@gmail.com>
2020-08-02 10:24:32 -0700

    V 3.3.2 - Update help command

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -28,27 +28,15 @@ export const help: commandInt = {
       .setColor("#ab47e6")
       .setTitle("Bot Commands")
       .setDescription(
-        `BEEP BOOP: Available commands include the following. Command name must be prefixed with '${prefix}', just like the '${prefix}help' command used to get this message. For a list of all commands, or for more information, view my [documentation](https://www.nhcarrigan.com/discord-bot-documentation)`
-      )
-      .addFields(
-        {
-          name: "about",
-          value: "Provides details about the bot.",
-        },
-        {
-          name: "ping",
-          value: "Measures the response time of the bot's server.",
-        },
-        {
-          name: "status",
-          value: "Provides details on the server.",
-        },
-        {
-          name: "user",
-          value: "Provides details on the user.",
-        }
+        `BEEP BOOP: Available commands include the following. Command name must be prefixed with '${prefix}', just like the '${prefix}help' command used to get this message. For information on a specific command, use '${prefix}help <command>.`
       )
       .setFooter("BEEP BOOP: End of message.");
+    const commandArray = [];
+    for (const command of commands) {
+      commandArray.push(`\`${command.prefix}\``);
+    }
+    const commandString = commandArray.sort().join(" | ");
+    helpEmbed.addFields({ name: "Available Commands", value: commandString });
     //DM the embed
     user.send(helpEmbed);
     //message to channel so people know bot is online
