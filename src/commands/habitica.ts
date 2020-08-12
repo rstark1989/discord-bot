4f427cab3bf0e215b0007e9e43cc02bf6e54165e
nhcarrigan <nhcarrigan@gmail.com>
2020-08-12 09:37:18 -0700

    Add achievements

--- src/commands/habitica.ts
+++ src/commands/habitica.ts
@@ -1,7 +1,10 @@
 import { CommandInt } from "../interfaces/CommandInt";
 import fetch from "node-fetch";
 import dotenv from "dotenv";
-import { HabiticaInt } from "../interfaces/HabiticaInt";
+import {
+  HabiticaUserInt,
+  HabiticaAchievementInt,
+} from "../interfaces/HabiticaInt";
 import { MessageEmbed } from "discord.js";
 dotenv.config();
 
@@ -23,11 +26,65 @@ export const habitica: CommandInt = {
         },
       }
     );
-    const habiticaData: HabiticaInt = await data.json();
+    const habiticaData: HabiticaUserInt = await data.json();
     if (!habiticaData.success) {
       message.channel.send("ERROR 404: User not found.");
       return;
     }
+    const achievementData = await fetch(
+      `https://habitica.com/api/v3/members/${cmdArguments}/achievements`,
+      {
+        method: "get",
+        headers: {
+          "x-client": "285a3335-33b9-473f-8d80-085c04f207bc-DiscordBot",
+          "x-api-user": "285a3335-33b9-473f-8d80-085c04f207bc",
+          "x-api-key": key,
+        },
+      }
+    );
+    const habiticaAchievementData: HabiticaAchievementInt = await achievementData.json();
+    if (!habiticaAchievementData.success) {
+      message.channel.send("ERROR 404: Achievements not found.");
+      return;
+    }
+    const basicAchievements = Object.keys(
+      habiticaAchievementData.data.basic.achievements
+    )
+      .filter(
+        (el: string) =>
+          habiticaAchievementData.data.basic.achievements[el].earned
+      )
+      .map((el) => habiticaAchievementData.data.basic.achievements[el].title)
+      .join(", ");
+    const onboardingAchievements = Object.keys(
+      habiticaAchievementData.data.onboarding.achievements
+    )
+      .filter(
+        (el: string) =>
+          habiticaAchievementData.data.onboarding.achievements[el].earned
+      )
+      .map(
+        (el) => habiticaAchievementData.data.onboarding.achievements[el].title
+      )
+      .join(", ");
+    const seasonalAchievements = Object.keys(
+      habiticaAchievementData.data.seasonal.achievements
+    )
+      .filter(
+        (el: string) =>
+          habiticaAchievementData.data.seasonal.achievements[el].earned
+      )
+      .map((el) => habiticaAchievementData.data.seasonal.achievements[el].title)
+      .join(", ");
+    const specialAchievements = Object.keys(
+      habiticaAchievementData.data.special.achievements
+    )
+      .filter(
+        (el: string) =>
+          habiticaAchievementData.data.special.achievements[el].earned
+      )
+      .map((el) => habiticaAchievementData.data.special.achievements[el].title)
+      .join(", ");
     const habiticaEmbed = new MessageEmbed()
       .setTitle(habiticaData.data.profile.name)
       .setDescription(`@${habiticaData.data.auth.local.username}`)
@@ -58,6 +115,16 @@ export const habitica: CommandInt = {
             .sort()
             .join(", "),
         },
+        { name: "Basic Achievements", value: basicAchievements || "None" },
+        {
+          name: "Onboarding Achievements",
+          value: onboardingAchievements || "None",
+        },
+        {
+          name: "Seasonal Achievements",
+          value: seasonalAchievements || "None",
+        },
+        { name: "Special Achievements", value: specialAchievements || "None" },
         {
           name: "Join Date",
           value: new Date(
